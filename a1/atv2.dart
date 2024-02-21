58b6ae04fb2b80ab4bbd03d9d966dd3353797d1e
nicole <133895033+nninishida@users.noreply.github.com>
2024-02-21 12:29:56 -0300

    Add files via upload

---
+++ a1/atv2.dart
@@ -0,0 +1,9 @@
+void main(){
+  var num = 20;
+
+  //Operador ternario
+  var mensagem = num % 2 == 0 ? 'Onúmero $num é par.' : 'O número $num é ímpar';
+
+  //imprimir a mensagem
+  print(mensagem);
+}
---
+++ a1/atv3.dart
@@ -0,0 +1,10 @@
+void mais(){
+  var num = 10;
+
+  //Estrutura condicional
+  if (num % 2 == 0) {
+    print('O número $num é par,');
+  } else {
+    print('O número $num é iímpar');
+  }
+}
